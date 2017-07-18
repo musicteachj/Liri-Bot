ad131258214cb1f3eb4e767294fa4b4f064ff383
musicteachj <jimilittle3@hotmail.com>
2017-07-17 22:33:29 -0400

    add it up!
    Liri Bot week 10 homework

---
+++ keys.js
@@ -0,0 +1,9 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'rYxdLWHeleuqORRIes8Hstnja',
+  consumer_secret: 'lEue8BX48BSn4a68bqRhyZVfl18EFJivDWZqPKWx3OrJsg3Cns',
+  access_token_key: '886271407038005248-CNWOzwMPsog8EU59iOpJrWrB2AumCCR',
+  access_token_secret: 'mkWDtWHFxLUCbd9yCdZZtOJrb8iM39LvIRnlnBtVAiynQ',
+}
+
